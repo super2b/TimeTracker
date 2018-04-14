21cecdeebef7599222e376d24e7d63cd56465576
super2b <super2b@qq.com>
2018-04-14 13:58:15 +0800

    update the homepage

--- timetracker_web/src/components/Home.vue
+++ timetracker_web/src/components/Home.vue
@@ -1,10 +1,5 @@
 <template>
-  <div class="jumbotron">
-    <h1>任务追踪</h1>
-    <p>
-      <strong>
-        <router-link to="/time-entries">创建一个任务</router-link>
-      </strong>
-    </p>
-  </div>
+  <b-jumbotron lead="任务追踪" >
+    <b-btn variant="primary" href="#">Add Task</b-btn>
+  </b-jumbotron>
 </template>
--- timetracker_web/src/main.js
+++ timetracker_web/src/main.js
@@ -14,6 +14,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App, BootstrapVue },
+  components: {App},
   template: '<App/>'
 })
