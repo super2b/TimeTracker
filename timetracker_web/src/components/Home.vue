813ef8d5b9990b58fe145a793072c45375af99fd
super2b <super2b@qq.com>
2018-04-15 10:59:47 +0800

    add basic buton onclick method

--- timetracker_web/src/components/Home.vue
+++ timetracker_web/src/components/Home.vue
@@ -1,19 +1,50 @@
 <template>
-  <h2>
-    <span>Pomodoro</span>
-    <b-button>
-      <font-awesome-icon :icon="icon"/>
-    </b-button>
-  </h2>
+  <div>
+    <h2>
+      <span>Task1</span>
+      <b-button @click="start()">
+        <font-awesome-icon :icon="icon"/>
+      </b-button>
+    </h2>
+    <div class="card card-body bg-light">
+      <div class="pomodoro-timer">
+        <span>{{ minute }}</span>:<span>{{ second }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 import faPlay from '@fortawesome/fontawesome-free-solid/faPlay'
+import Const from './Constant'
 export default {
   name: 'FAExample',
   computed: {
     icon () {
       return faPlay
+    }
+  },
+  data () {
+    return {
+      minute: Const.RESTING_TIME_LENGTH_IN_MINUTES,
+      second: 0
+    }
+  },
+  methods: {
+    start: function () {
+      this._tick()
+      this.interval = setInterval(this._tick, 1000)
+    },
+    _tick: function () {
+      if (this.second !== 0) {
+        this.second--
+        return
+      }
+
+      if (this.minute !== 0) {
+        this.minute--
+        this.second = 59
+      }
     }
   },
   components: {
