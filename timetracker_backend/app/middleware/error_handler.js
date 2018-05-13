23a90d82515b9538ca552cc6c42153ccd904ca03
super2b <super2b@qq.com>
2018-05-13 23:36:28 +0800

    404错误处理的状态码调整

--- timetracker_backend/app/middleware/error_handler.js
+++ timetracker_backend/app/middleware/error_handler.js
@@ -2,9 +2,10 @@ module.exports = () => {
   return async function errorHandler(ctx, next) {
     await next();
     if (ctx.status === 404) {
+      ctx.status = 404
       ctx.body = { 
         success: false,
-        msg: 'not found222'
+        msg: 'the function not supported.'
        };
     } 
   };
--- timetracker_backend/config/config.default.js
+++ timetracker_backend/config/config.default.js
@@ -11,7 +11,7 @@ module.exports = appInfo => {
     mysql: {
       client: {
         // host
-        host: '192.168.0.104',
+        host: '192.168.0.106',
         // 端口号
         port: '3306',
         // 用户名
@@ -29,7 +29,7 @@ module.exports = appInfo => {
     redis: {
       client: {
         port: 6379,
-        host: '192.168.0.104',
+        host: '192.168.0.106',
         password: '123456',
         db: 0
       }
